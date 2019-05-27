0e051c12737a683aa84eb1bac7ad56055105f474
raul sanz <estraulsanz@gmail.com>
2019-05-26 22:00:05 -0500

    make it bette

--- app/Controllers/Http/SaleController.js
+++ app/Controllers/Http/SaleController.js
@@ -7,37 +7,33 @@ class SaleController {
 
     const user = await auth.getUser()
 
-    const products = await Product.query().where('company_id', user.company_id).fetch()
+    const sales = await Sale.query().where('company_id', user.company_id).fetch()
 
     return response.json({
       status: 'succes',
-      data: products
+      data: sales
     })
   }
 
 
   async store ({ request, response }) {
+    
     try {
-      const product = await Product.create({
-        category_id:request.input('category_id'),
-        name:request.input('name'),
-        price:request.input('price'),
-        code:request.input('code'),
-        stock:request.input('stock'),
-        description:request.input('description'),
-        type:request.input('type'),
-        model:request.input('model'),
-        color:request.input('color'),
-        brand:request.input('brand'),
-        max_stock:request.input('max'),
-        min_stock:request.input('min'),
-        company_id:request.input('company_id')
+      
+      const sale = await Sale.create({
+        company_id:request.input('company_id'),
+        subtotal:request.input('subtotal'),
+        iva:request.input('iva'),
+        total:request.input('total'),
+        ticket:request.input('ticket'),
+        seller:request.input('seller'),
+        products:request.input('products')
       })
 
       return response.json({
         status: 'success',
-        data: product
-      })
+        data: sale
+      }) 
     } catch (error) {
       console.log(error);
       return response.status(400).json({
@@ -49,56 +45,51 @@ class SaleController {
 
  
   async show ({ params, request, response, view }) {
-    const product = await Product.find(params.id)
+    const sale = await Sale.find(params.id)
 
-    if (!product) {
-      return response.status(404).json({status:'error', message:'Producto no encontrado'})
+    if (!sale) {
+      return response.status(404).json({status:'error', message:'Saleo no encontrado'})
     }
 
     return response.json({
       status: 'success',
-      data:product
+      data:sale
     })
   }
 
 
   async update ({ params, request, response }) {
-    const product = await Product.find(params.id)
+    const sale = await Sale.find(params.id)
 
-    if ( !product ) {
-      return response.status(404).json({status:'error', message:'Producto no encontrado'}) 
+    if ( !sale ) {
+      return response.status(404).json({status:'error', message:'Saleo no encontrado'}) 
     }
     
-    product.category_id = request.input('category_id'),
-    product.name = request.input('name'),
-    product.price = request.input('price'),
-    product.code = request.input('code'),
-    product.stock = request.input('stock'),
-    product.description = request.input('description'),
-    product.type = request.input('type'),
-    product.model = request.input('model'),
-    product.color = request.input('color'),
-    product.brand = request.input('brand'),
-    product.max_stock = request.input('max'),
-    product.min_stock = request.input('min'),
+    sale.company_id = request.input('company_id'),
+    sale.subtotal = request.input('subtotal'),
+    sale.iva = request.input('iva'),
+    sale.total = request.input('total'),
+    sale.ticket = request.input('ticket'),
+    sale.seller = request.input('seller'),
+    sale.products = request.input('products')
 
-    await product.save()
+    await sale.save()
 
     return response.status(201).json({
       status: 'success',
-      data: product
+      data: sale
     })
   }
 
 
   async destroy ({ params, request, response }) {
-    const product = await Product.find(params.id)
+    const sale = await Sale.find(params.id)
 
-    if (!product) {
-      return response.status(404).json({status:'error', message:'Producto no encontrado'})
+    if (!sale) {
+      return response.status(404).json({status:'error', message:'Saleo no encontrado'})
     }
 
-    product.delete()
+    sale.delete()
 
     return response.status(204).json({
       staus:'success',
